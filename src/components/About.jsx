ab1f64dcba42d94bd2ab634e5b2ca090e801a3f1
regnfylt <148742788+regnfylt@users.noreply.github.com>
2024-04-22 10:14:07 +0300

    About alustava tieto

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,18 @@
 export default function About() {
-    return (<h1>About us: CHIPI CHIPI CHAPA CHAPA</h1>);
-  }
+    return (
+        <div>
+            <h1>About us: CHIPI CHIPI CHAPA CHAPA</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum esse voluptates, deserunt ea repudiandae voluptatum architecto omnis nulla sint odio possimus. Cupiditate error sunt tempora eligendi in cumque est, impedit libero illo consectetur molestiae aut ipsa accusantium perferendis culpa voluptatem repudiandae officiis nulla beatae, eaque aliquid alias eveniet. Earum, beatae?</p>
+            <div>
+                <p>Toimitusjohtaja: Erkki Versti</p>
+                <p>Markkinointikoordinaattori: Markku Arkkina</p>
+                <p>Hankintajohtaja: Heidi Ankkija</p>
+            </div>
+            <div>
+                <p>Yrityksen perustamisvuosi: 2021</p>
+                <p>Y-tunnus: FIN-024</p>
+            </div>
+        </div>
+
+    );
+}
