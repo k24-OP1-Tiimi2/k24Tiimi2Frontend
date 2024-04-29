b0ec15290ca14044ab9e487c054ad1e960585265
regnfylt <148742788+regnfylt@users.noreply.github.com>
2024-04-29 09:47:34 +0300

    nav styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Link, Outlet } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <nav>
+      <nav className="nav">
         <Link to={"/"}>Home</Link>
         <Link to={"/about"}>About</Link>
         <Link to={"/products"}>Products</Link>
